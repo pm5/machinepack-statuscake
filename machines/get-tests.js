45ddda4e016c6a766dd55b9ff1935539bac5c0dd
Pomin Wu <pomin5@gmail.com>
2015-04-04 13:17:05 +0800

    refactor and add more error detection

--- machines/get-tests.js
+++ machines/get-tests.js
@@ -1,8 +1,8 @@
 module.exports = {
-  friendlyName: 'Get Tests details',
-  description: 'Get detail information about a Test.',
+  friendlyName: 'Get Tests',
+  description: 'Get all Tests connected to your StatusCake account.',
   extendedDescription: '',
-  moreInfoUrl: 'http://statuscake.com/api/Tests/Get%20Detailed%20Test%20Data.md',
+  moreInfoUrl: 'http://statuscake.com/api/Tests/Get%20All%20Tests.md',
   cacheable: true,
   inputs: {
     statusCakeUser: {
@@ -13,13 +13,12 @@ module.exports = {
     apiKey: {
       example: 'phae6aesa5ohgh1vov7aeM7Kaec2eeca',
       description: 'Your StatusCake API key.',
-      required: true
-    },
-    id: {
-      example: 34567,
-      description: 'ID of the StatusCake Test.',
-      required: true
-    },
+      required: true,
+      whereToGet: {
+        url: 'https://www.statuscake.com/App/APIKey.php',
+        description: 'Copy "Below is your API Key" from your dashboard.'
+      },
+    }
   },
   defaultExit: 'success',
   exits: {
@@ -30,7 +29,21 @@ module.exports = {
       description: 'Invalid or unprovided API key or username. All calls must have a valid StatusCake API key and username.'
     },
     success: {
-      description: 'Returns information about a Test.',
+      description: 'Returns all your Tests.',
+      example: [
+        {
+          TestID: 378573,
+          Paused: false,
+          TestType: 'HTTP',
+          WebsiteName: 'node-statuscake test',
+          ContactGroup: null,
+          ContactID: 0,
+          Public: 0,
+          Status: 'Up',
+          NormalisedResponse: 0,
+          Uptime: 100
+        }
+      ]
     },
   },
   fn: function (inputs, exits) {
@@ -38,8 +51,10 @@ module.exports = {
     statuscake
       .username(inputs.statusCakeUser)
       .key(inputs.apiKey)
-      .testsDetails(inputs.id, function (err, data) {
-        if (err) return exits.error(err);
+      .tests(function (err, data) {
+        if (err) {
+          return exits.error(err);
+        }
         if (undefined !== data.ErrNo && 0 === data.ErrNo) {
           return exits.wrongOrNotUser(data.Error);
         }
