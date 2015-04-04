1552eef3d54ae1d490986be37185a6cd5f5d87c7
Pomin Wu <pomin5@gmail.com>
2015-04-04 12:52:51 +0800

    add get-tests-details machine

--- machines/get-tests-details.js
+++ machines/get-tests-details.js
@@ -1,6 +1,6 @@
 module.exports = {
-  friendlyName: 'Get all Tests',
-  description: 'Get all Tests connected to your StatusCake account.',
+  friendlyName: 'Get Tests details',
+  description: 'Get detail information about a Test.',
   extendedDescription: '',
   inputs: {
     statusCakeUser: {
@@ -12,30 +12,29 @@ module.exports = {
       example: 'phae6aesa5ohgh1vov7aeM7Kaec2eeca',
       description: 'Your StatusCake API key.',
       required: true
-    }
+    },
+    id: {
+      example: 34567,
+      description: 'ID of the StatusCake Test.',
+      required: true
+    },
   },
   defaultExit: 'success',
   exits: {
     error: {
       description: 'Unexpected error occurred.',
     },
-    wrongOrNoKey: {
-      description: 'Invalid or unprovided API key. All calls must have a key.'
-    },
     success: {
-      description: 'Returns all your Tests.',
-      example: " [ { TestID: 378573, Paused: false, TestType: 'HTTP', WebsiteName: 'node-statuscake test', ContactGroup: null, ContactID: 0, Public: 0, Status: 'Up', NormalisedResponse: 0, Uptime: 100 } ]"
+      description: 'Returns information about a Test.',
     },
   },
-  fn: function (inputs, exits) {
+  fn: function (inputs,exits) {
     var statuscake = require("statuscake");
     statuscake
       .username(inputs.statusCakeUser)
       .key(inputs.apiKey)
-      .tests(function (err, data) {
-        if (err) {
-          return exits.error(err);
-        }
+      .testsDetails(inputs.id, function (err, data) {
+        if (err) return exits.error(err);
         return exits.success(data);
       });
   },
